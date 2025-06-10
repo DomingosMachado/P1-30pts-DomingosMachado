41be9ef719a20d44194a822f2ef27ed89c0cbba2
Domingos Machado <d.machado1993@gmail.com>
2025-06-09 21:16:25 -0300

    Perfumaria

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -6,14 +6,15 @@ function Home() {
       <h1 className={styles.title}>Exercício React</h1>
       <p className={styles.subtitle}>Bem-vindo! Escolha uma das questões abaixo:</p>
       
-      <div className={styles.linksContainer}>        <a href="/questao1" className={`${styles.link} ${styles.linkBlue}`}>
+      <div className={styles.linksContainer}>
+        <a href="/questao1" className={`${styles.link} ${styles.linkBlue}`}>
           Questão 1 - Tema Claro/Escuro
         </a>
         
         <a href="/questao2" className={`${styles.link} ${styles.linkGreen}`}>
           Questão 2 - Mensagem de Boas-Vindas
-        </a>
-        
+        </a>       
+         
         <a href="/questao3" className={`${styles.link} ${styles.linkYellow}`}>
           Questão 3 - Lista de Tarefas
         </a>
